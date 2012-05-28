71c5fcf17fcbd4e17e292b7fe0435bd2ef393909
John Duff <john.duff@jadedpixel.com>
2012-05-28 15:52:37 -0400

    Add thread worker tests, refactor workers

--- lib/forever_rails/forever_worker_base.rb
+++ lib/forever_rails/forever_worker_base.rb
@@ -1,14 +1,16 @@
 require "active_support/core_ext/class/attribute_accessors"
+require "logger"
 
 class ForeverWorkerBase
   cattr_accessor :timespan
+
   def self.every(time)
     @@timespan = time  
   end
 
-  attr_accessor :stopped, :logger, :thread
-  def initialize(options)
-    @logger = options[:logger]    
+  attr_accessor :logger
+  def initialize(options={})
+    @logger = options[:logger] || Logger.new(STDOUT)
     @stopped = false
   end
 
@@ -16,30 +18,32 @@ class ForeverWorkerBase
     @stopped = true
   end
 
+  def stopped?
+    @stopped
+  end
+
   # override this in subclass to implement a single run
   def run
+    raise NotImplementedError
   end
 
   def run_forever
-    @thread = Thread.new do
-      last_run = 0
-      while !stopped
-        now = Time.now.to_i
-        if now - last_run > @@timespan
-          last_run = now
-          begin
-            run
-          rescue StandardError => e
-            logger.error("Caught #{e.class.name} while running #{self.class.name}: #{e.message}\n--  #{e.backtrace.join("\n--  ")}")
-          end
-        else
-          sleep 1
+    last_run = nil
+    while !stopped?
+      now = Time.now.to_i
+      if !last_run || (now - last_run > timespan)
+        last_run = now
+        begin
+          run
+        rescue StandardError => e
+          logger.error("Caught #{e.class.name} while running #{self.class.name}: #{e.message}\n--  #{e.backtrace.join("\n--  ")}")
         end
+      else
+        sleep 1
       end
     end
   end
 
   def wait_for_quit
-    @thread.join
   end
 end
---
+++ test/forever_fork_worker_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ForeverForkWorkerTest < Test::Unit::TestCase
+end
